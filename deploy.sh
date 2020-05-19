80650ceb8fb7eb143820fea60297663370f3dfa2
Tom Romanowski <TomRomanowski7@gmail.com>
2020-05-19 19:13:09 -0400

    Add travis config

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+# Build images
+docker build -t tomromanowski7/multi-client:latest -t tomromanowski7/multi-client:"$SHA" -f ./client/Dockerfile ./client
+docker build -t tomromanowski7/multi-server:latest -t tomromanowski7/multi-server:"$SHA" -f ./server/Dockerfile ./server
+docker build -t tomromanowski7/multi-worker:latest -t tomromanowski7/multi-worker:"$SHA" -f ./worker/Dockerfile ./worker
+
+# Push images
+docker push tomromanowski7/multi-client:latest
+docker push tomromanowski7/multi-client:"$SHA"
+docker push tomromanowski7/multi-server:latest
+docker push tomromanowski7/multi-server:"$SHA"
+docker push tomromanowski7/multi-worker:latest
+docker push tomromanowski7/multi-worker:"$SHA"
+
+# Apply kubernetes configs
+kubectl apply -f k8s
+
+# Update images
+kubectl set image deployments/client-deployment server=tomromanowski7/multi-client:"$SHA"
+kubectl set image deployments/server-deployment server=tomromanowski7/multi-server:"$SHA"
+kubectl set image deployments/worker-deployment server=tomromanowski7/multi-worker:"$SHA"
